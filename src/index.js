506b36fc07dd771b7cd4d11bc5b685bdc68014b2
mwc <1819293933@qq.com>
2019-06-29 19:48:14 +0800

    大拆分

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import FocusFlow from './ff/constructor'
 import pond from './ff/pond'
 import threads from './ff/threads'
+import transfer from './ff/transfer'
 let prototype = FocusFlow.prototype
 const obj = {
   ...pond,
-  ...threads
+  ...threads,
+  ...transfer
 }
 Object.keys(obj).forEach(key => {
   Object.defineProperty(prototype, key, {
